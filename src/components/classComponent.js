f05b45fe75e026f369a6e1362f5f7b1cae28d162
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-23 16:40:14 +0530

    Toogle method binding 1

--- src/components/classComponent.js
+++ src/components/classComponent.js
@@ -3,11 +3,22 @@ import React from "react";
 class ProductCatalogClass extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            isLoggedIn : false
+        }
+        this.togglePage=this.togglePage.bind(this);
+    }
+    togglePage(){
+        //this.state.isLoggedIn = !this.state.isLoggedIn;  //you can't do this
+        this.setState({isLoggedIn: !this.state.isLoggedIn})
     }
     render(){
         return(
-            <>
-            </>
+            <div>
+                <button onClick={this.togglePage}>Sign In</button>
+                <button onClick={this.togglePage}>Sign Out</button>
+                {this.state.isLoggedIn ? <p>True</p> : <p>False</p>}
+            </div>
         )
     }
 } 
---
+++ src/components/isLoggedOut.js
@@ -0,0 +1,9 @@
+const LogoutPage=()=>{
+    return(
+        <div>
+            <h2>Login Page</h2>
+        </div>
+    )
+}
+
+export default LogoutPage;
