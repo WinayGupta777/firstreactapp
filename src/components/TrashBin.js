8ff4ab03da403048a5ab14ffe3ab24d428451b63
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-24 18:44:07 +0530

    Created Trash Bin component

---
+++ src/components/TrashBin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Trash extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){return(
+        <>
+            <h2>Trash Bin</h2>
+        </>
+    )}
+}
+
+export default Trash;
