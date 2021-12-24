7a4ae15428a986dcd456672360673d208f4f6bc5
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-24 23:48:05 +0530

    Just added Clear Button!!

--- src/components/TrashBin.js
+++ src/components/TrashBin.js
@@ -4,9 +4,14 @@ class Trash extends React.Component {
     constructor(props){
         super(props)
     }
+    clearALL=()=>{
+        
+    }
     render(){return(
         <>
-            <h2>Trash Bin</h2>
+            <h2>Trash Bin  
+                <button>Clear</button>
+            </h2>
             {this.props.list.map(
                 (i)=>
                 <div>
