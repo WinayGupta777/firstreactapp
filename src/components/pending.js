c2f92c0bf1359bc1fdb15c38054833563c176c6d
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-28 19:36:15 +0530

    Another method with const!

--- src/components/pending.js
+++ src/components/pending.js
@@ -3,12 +3,17 @@ class Pending extends React.Component {
     constructor(props){
         super(props)
     }
-    render(){return(
+    render(){
+        const divStyle = {
+            color: "orange",
+            fontFamily: "monospace",
+        }
+        return(
         <>
             <h2  style={{backgroundColor:"red"}}>Pending Tasks</h2>
             {this.props.list.map(
                 (i)=>
-                <div>
+                <div style={divStyle}>
                     <p>{i.title}</p>
                     <button onClick={()=>this.props.fun(i.id)}>Done</button>
                     <button onClick={()=>this.props.fun2(i.id)}>Delete</button>
