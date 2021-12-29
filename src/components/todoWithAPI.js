ece700398d202fadf940aa912aa746916fa71570
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-29 12:34:35 +0530

    Tried to extract data from object, then from the completed status We sent it to Pending Complonent

--- src/components/todoWithAPI.js
+++ src/components/todoWithAPI.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Pending from "./pending";
 class TodoApi extends React.Component {
     constructor(props){
         super(props)
@@ -7,9 +7,32 @@ class TodoApi extends React.Component {
             todos:[]
         }
     }
+    componentDidMount=()=>{
+        fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((result)=>result.json())
+        .then((jsonData)=>this.setState({todos:jsonData}))
+    }
+    getPending=()=>{
+        let pends = this.state.todos.filter(
+            (i)=>i.completed===true
+        );
+        return pends;
+    }
     render(){
         return(
-            <h3>{this.state.todos}</h3>
+            <>
+                <Pending list={this.getPending()}   fun={this.changeState}  fun2={this.move2trash}></Pending>
+            {/* <h3>
+                {this.state.todos.map(
+                    (i)=>
+                    <>
+                        <p>Id:{i.id}</p>
+                        <p>Title:{i.title}</p>
+                        <p>Completed:{i.completed}</p>
+                    </>
+                )}
+            </h3> */}
+            </>
         )
     }
 } 
