08d8f7cbf23c3d7c1e994081cd55bae9e77ff4c5
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-29 11:08:27 +0530

    Showed in brower, & now will work on LifeCycle Methods!!

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@
 import ProductCatalogClass from "./components/classComponent";
 import AddItem from "./components/addCounter";
 import TodoComponent from "./components/TodoComponent";
+
+import LifeCycle from "./components/lifeCycle";
 const App=()=>{
   return(
     <>
-      <TodoComponent></TodoComponent>
+      <LifeCycle></LifeCycle>
     </>
   )
 }
